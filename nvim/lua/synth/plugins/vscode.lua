e160159e19646d0e707294d23a7fb160997c9226
synth-tm <chrisharris4418@gmail.com>
2026-05-01 19:30:01 -0700

    Update vscode.lua
    theme now functions

--- nvim/lua/synth/plugins/vscode.lua
+++ nvim/lua/synth/plugins/vscode.lua
@@ -1,24 +1,24 @@
-local function enable_transparency()
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-end
-local function italic_comments()
-    vim.api.nvim_set_hl(0, 'Comment', { italic=true })
-end
-local function underline_links()
-    vim.api.nvim_set_hl(0, "@markup.link", { underline = true })
-    vim.api.nvim_set_hl(0, "@markup.link.url", { underline = true })
-end    
+local c = require('vscode.colors').get_colors()
+
 return {
     {
         "mofiqul/vscode.nvim",
-        priority = 1000,
-        config = function()
-            require('vscode').setup({
-                enable_transparency(),
-                italic_comments(),
-                underline_links(),
-            })
-        end
+        require('vscode').setup({
+            priority = 1000,
+            transparent = true,
+            italic_comments = true,
+            italic_inlayhints = true,
+            underline_links = true,
+            disable_nvimtree_bg = true,
+            terminal_colors = true,
+            color_overrides = {
+                vscLineNumber = '#FFFFFF',
+            },
+            group_overrides = {
+                Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+            }
+        }),
+        require('vscode').load('dark')
     },
     {
         "nvim-lualine/lualine.nvim",
